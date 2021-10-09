e837d10cbf9b68c2ab748a4cfaf50538becc74e2
oussama0X0X4 <92224698+oussama0X0X4@users.noreply.github.com>
2021-10-09 18:18:39 +0100

    oussama.py

--- oussama.py
+++ oussama.py
@@ -4,23 +4,12 @@ try:
     import time 
 except Exception as e: 
     print(e) 
-import pyfiglet
-
-# = = = = = = = = = = = = 
-
-Z = '\033[1;31m' #احمر
-X = '\033[1;33m' #اصفر
-Z1 = '\033[2;31m' #احمر ثاني
-F = '\033[2;32m' #اخضر
-A = '\033[2;34m'#ازرق
-C = '\033[2;35m' #وردي
-B = '\033[2;36m'#سمائي
-Y = '\033[1;34m' #ازرق فاتح
-
-# = = = = = = = = = = = =
-
-logo = pyfiglet.figlet_format(' *OUSSAMA 0X0X4 *')
-print(X+logo)
+logo = ("""
+                             0X0X4
+          |--------------------------|
+          |       OUSSAMA 0X0X4
+          |--------------------------|
+""")
  
 print(logo) 
 user = input('Enter username: ') 
